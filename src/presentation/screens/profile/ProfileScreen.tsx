f9120a33f104fff333d56490a474fe68b56f616a
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-06-25 17:11:59 -0500

    134. Cambios en el Store

--- src/presentation/screens/profile/ProfileScreen.tsx
+++ src/presentation/screens/profile/ProfileScreen.tsx
@@ -1,15 +1,34 @@
-import { Text, View } from 'react-native';
+import { Pressable, Text, View } from 'react-native';
 import { styles } from '../../../config';
 import { useProfileStore } from '../../store/profile-store';
 
 export const ProfileScreen = () => {
 	const name = useProfileStore(state => state.name);
 	const email = useProfileStore(state => state.email);
+	const changeProfile = useProfileStore(state => state.changeProfile);
 
 	return (
 		<View style={styles.container}>
 			<Text style={styles.title}>{name}</Text>
 			<Text style={styles.title}>{email}</Text>
+
+			<Pressable
+				style={styles.primaryButton}
+				onPress={() => useProfileStore.setState({ name: 'Harold Gonzalez' })}>
+				<Text>Cambiar nombre</Text>
+			</Pressable>
+
+			<Pressable
+				style={styles.primaryButton}
+				onPress={() => useProfileStore.setState({ email: 'harold@gmail.com' })}>
+				<Text>Cambiar Email</Text>
+			</Pressable>
+
+			<Pressable
+				style={styles.primaryButton}
+				onPress={() => changeProfile('John Doe', 'john.doe@google.com')}>
+				<Text>Regresa a John</Text>
+			</Pressable>
 		</View>
 	);
 };
