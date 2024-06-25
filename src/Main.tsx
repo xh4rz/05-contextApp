559e24cd008b0f8c68d2095ba03bac6cda2122cf
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-06-25 16:56:40 -0500

    131. Preparación de la aplicación y tarea 132. Resolución de la tarea

---
+++ src/Main.tsx
@@ -0,0 +1,10 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { BottomTabNavigator } from './presentation/navigators/BottomTabNavigator';
+
+export const Main = () => {
+	return (
+		<NavigationContainer>
+			<BottomTabNavigator />
+		</NavigationContainer>
+	);
+};
---
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+import { styles } from '../../../config';
+
+export const HomeScreen = () => {
+	return (
+		<View style={styles.container}>
+			<Text>HomeScreen</Text>
+		</View>
+	);
+};
